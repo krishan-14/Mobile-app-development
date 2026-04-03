7b37c9c2b0632c80e6829ebc9c9c6ac62d69b794
Krishan <kk9310492@gmail.com>
2026-04-03 16:08:44 +0530

    push 2 projects

--- app/src/main/java/com/example/mad_q1/MainActivity.java
+++ app/src/main/java/com/example/mad_q1/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private String[] currencies = {"INR", "USD", "JPY", "EUR"};
 
     @Override
+    //project
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
